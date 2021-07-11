89bd98332cced9451fa1ea071d6f6c8e5fc64d93
Shenzhen YAO <junk.them@hotmail.com>
2021-07-11 08:36:31 -0600

    v1

---
+++ python/bridge.py
@@ -0,0 +1,15 @@
+# https://www.ted.com/talks/alex_gendler_can_you_solve_the_bridge_riddle/transcript?language=en
+
+# four persons
+speed = {
+    "you": 1,
+    "assistant":2,
+    "janiter":5,
+    "professor":10
+}
+
+left = ["you", "assistant", "janiter", "professor"]
+right =[]
+time=0
+
+print(speed, left, right, time)
